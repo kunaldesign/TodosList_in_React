40ea44ec1d09c038ebe174992690f4560df20251
KunalHedaoo <kunalhedaoo25@gmail.com>
2021-09-03 02:10:48 -0400

    v.1.0

---
+++ src/MyComponents/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Nesciunt, fuga. Illum quidem incidunt, aspernatur corporis illo,
+                aliquam autem officia vitae, omnis ipsum ullam? Facere sequi ipsa,
+                provident eveniet deleniti ipsam?</p>
+        </div>
+    )
+}
